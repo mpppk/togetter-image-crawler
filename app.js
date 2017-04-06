850c552393fcf9713a152f14095c2b66335d52e2
mpppk <niboshiporipori@gmail.com>
2017-04-07 00:48:38 +0900

    Wrap by co

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ const path = require('path');
 
 const setTimeoutAsync = delay => new Promise(r => setTimeout(r, delay));
 
-client.fetch('https://togetter.com/li/1088229')
-  .then(result => {
+const getTogetterImages = togetterUrl => {
+  co(function *() {
+    const result = yield client.fetch(togetterUrl);
     const $ = result.$;
-    return $('.list_photo img')
+    const urls = $('.list_photo img')
       .map((i, d) => $(d).attr('src'))
       .get();
-  }, e => console.log(e))
-  .then(urls => {
-    return co(function *() {
-      for(let i in urls) {
-        const url = urls[i];
-        const fileName = path.basename(url).split(':')[0];
-        console.log(`${i+1}/${urls.length} ${fileName}`);
-        const response = yield axios.get(url, {responseType: 'stream'});
-        response.data.pipe(fs.createWriteStream(fileName));
-        yield setTimeoutAsync(1000);
-      }
-    });
-  }, e => console.log(e));
+
+    for(let i in urls) {
+      const url = urls[i];
+      const fileName = path.basename(url).split(':')[0];
+      console.log(`${+i+1}/${urls.length} ${fileName}`);
+      const response = yield axios.get(url, {responseType: 'stream'});
+      response.data.pipe(fs.createWriteStream(fileName));
+      yield setTimeoutAsync(1000);
+    }
+  });
+};
+
+getTogetterImages('https://togetter.com/li/1088229');
