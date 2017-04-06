cbe569e0cb1af36d6b6aa641ca3b6ac6fa9f99c8
mpppk <niboshiporipori@gmail.com>
2017-04-07 02:57:37 +0900

    Support multiple pages

--- app.js
+++ app.js
@@ -4,28 +4,44 @@ const fs = require('fs');
 const path = require('path');
 const Nightmare = require('nightmare');
 
-let nightmare = Nightmare();
-
 const setTimeoutAsync = delay => new Promise(r => setTimeout(r, delay));
 
-const clickMoreButton = (nightmare, togetterUrl) => {
+const hasMoreButton = (nightmare) => {
+    return nightmare.exists('.more_tweet_box a.btn');
+};
+
+const clickMoreButton = (nightmare) => {
   return nightmare
-    .goto(togetterUrl)
     .click('.more_tweet_box a.btn')
     .wait('.pagenation')
 };
 
-const getTogetterImages = (togetterUrl, interval) => {
-  co(function *() {
-    console.log(`fetching... [${togetterUrl}]`);
+const clickMoreButtonIfExist = (nightmare) => {
+  return co(function *() {
+    if(yield hasMoreButton(nightmare)) {
+      yield clickMoreButton(nightmare);
+    }
+  });
+};
+
+const getTogetterImageUrls = (togetterUrl) => {
+  return co(function *() {
+    console.log(`fetching image urls... [${togetterUrl}]`);
+    const nightmare = Nightmare();
+    yield nightmare.goto(togetterUrl);
+    yield clickMoreButtonIfExist(nightmare);
     const urls =
-      yield clickMoreButton(nightmare, togetterUrl)
-      .evaluate(() => {
-        const nodeList =  document.querySelectorAll('.list_photo img');
+      yield nightmare.evaluate(() => {
+        const nodeList = document.querySelectorAll('.list_photo img');
         return Array.prototype.map.call(nodeList, (node) => node.src);
       });
     yield nightmare.end();
+    return urls;
+  });
+};
 
+const getImages = (urls, interval) => {
+  return co(function *() {
     for(let i in urls) {
       const url = urls[i];
       const fileName = path.basename(url).split(':')[0];
@@ -37,12 +53,21 @@ const getTogetterImages = (togetterUrl, interval) => {
   });
 };
 
+const getTogetterImages = (togetterUrl, interval) => {
+  return co(function *() {
+    const urls = yield getTogetterImageUrls(togetterUrl);
+    return yield getImages(urls, interval);
+  });
+};
+
 const getMaxPage = (togetterUrl) => {
   return co(function *() {
-    console.log(`fetching... [${togetterUrl}]`);
+    const nightmare = Nightmare();
+    yield nightmare.goto(togetterUrl);
+    console.log(`fetching max page... [${togetterUrl}]`);
+    yield clickMoreButtonIfExist(nightmare);
     const pageLinks =
-      yield clickMoreButton(nightmare, togetterUrl)
-      .evaluate(() => {
+      yield nightmare.evaluate(() => {
         const nodeList =  document.querySelectorAll('.pagenation a');
         return Array.prototype.map.call(nodeList, (node) => node.textContent);
       });
@@ -54,7 +79,11 @@ const getMaxPage = (togetterUrl) => {
   });
 };
 
-getMaxPage('https://togetter.com/li/1088229')
-  .then(page => console.log(page));
-// getTogetterImages('https://togetter.com/li/1088229', 2000);
-// getTogetterImages('https://togetter.com/li/1088229?page=2', 2000);
+co(function *() {
+  const maxPage = yield getMaxPage('https://togetter.com/li/1088229');
+  for(let i = 0; i < maxPage; i++) {
+    console.log(`page ${i+1}/${maxPage}`);
+    yield getTogetterImages('https://togetter.com/li/1088229?page=' + (i+1), 2000);
+  }
+})
+  .then(() => console.log('finished!'), e => console.log(e));
