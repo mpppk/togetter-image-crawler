d52df24c690e5575f503d03cfda25a92f767f92e
mpppk <niboshiporipori@gmail.com>
2017-04-07 00:31:39 +0900

    Add image saving function

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const client = require('cheerio-httpcli');
+const co = require('co');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const setTimeoutAsync = delay => new Promise(r => setTimeout(r, delay));
 
 client.fetch('https://togetter.com/li/1088229')
   .then(result => {
     const $ = result.$;
-    const list = $('.list_photo img')
+    return $('.list_photo img')
       .map((i, d) => $(d).attr('src'))
       .get();
-    console.log(list);
-  });
-git
+  }, e => console.log(e))
+  .then(urls => {
+    return co(function *() {
+      for(let i in urls) {
+        const url = urls[i];
+        const fileName = path.basename(url).split(':')[0];
+        console.log(`${i+1}/${urls.length} ${fileName}`);
+        const response = yield axios.get(url, {responseType: 'stream'});
+        response.data.pipe(fs.createWriteStream(fileName));
+        yield setTimeoutAsync(1000);
+      }
+    });
+  }, e => console.log(e));
